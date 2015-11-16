a8ee38528d982a67d53d407bcd1edddb824d03d6
ari-trumpet <brassband_160trumpet@yahoo.co.jp>
2015-11-16 15:32:26 +0900

    Update fadd.c

--- fadd.c
+++ fadd.c
@@ -13,19 +13,14 @@ uint32_t fadd(uint32_t a, uint32_t b)
     uint32_t i;
     
     if (EXP(a) == 0xff || EXP(b) == 0xff) {
-        
         if (ISNaN(a) || ISNaN(b)) return (ISNaN(a) ? a : b) | (1 << 22);
-        
         if (ISINF(a) && ISINF(b)) return a == b ? a : 0xffc00000;
-        
         return EXP(a) == 0xff ? a : b;
     }
     
    
     if (EXP(a) == 0 || EXP(b) == 0) {
-        
         if (EXP(a) == 0 && EXP(b) == 0) {
-            
             if (MAN(a) == MAN(b)) return SIGN(a) & SIGN(b);
             return MAN(a) < MAN(b) ? SIGN(b) : SIGN(a);
         }
@@ -41,13 +36,10 @@ uint32_t fadd(uint32_t a, uint32_t b)
     uint32_t sign = SIGN(a);
     uint32_t exp  = EXP(a);
     
-    
     if (EXP(a) - EXP(b) >= 32) return a;
-    
     
     uint32_t ma = (1 << 26) | (MAN(a) << 3);
     uint32_t mb = (1 << 26) | (MAN(b) << 3);
-    
     
     for (i = 0; i < EXP(a) - EXP(b); ++i)
         mb = (mb >> 1) | (mb & 1);
