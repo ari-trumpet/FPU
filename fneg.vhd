5aca529cc083a3da3a886d64caa1252411f4d8b8
ari-trumpet <brassband_160trumpet@yahoo.co.jp>
2015-11-29 06:18:25 +0900

    Update fneg.vhd

--- fneg.vhd
+++ fneg.vhd
@@ -4,19 +4,14 @@ use ieee.std_logic_arith.ALL;
 use ieee.std_logic_unsigned.ALL;
 
 entity fneg is
-  
   port (
     a : in std_logic_vector(31 downto 0);
     s : out std_logic_vector(31 downto 0)
   );
-
 end fneg;
 
 architecture behavior of fneg is
-  
-begin  -- behave
-
+begin  -- behavior
   s(30 downto 0) <= a(30 downto 0);
   s(31) <= not a(31);
-  
 end behavior;
