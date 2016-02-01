d8865cc7fc569aa94d75f4e1712e7729061bcb77
ari-trumpet <brassband_160trumpet@yahoo.co.jp>
2016-02-01 18:37:45 +0900

    Update trnc.vhd

--- trnc.vhd
+++ trnc.vhd
@@ -5,16 +5,16 @@ use ieee.numeric_std.all;
 library work;
 use work.fpu_common_p.all;
 
-entity ftoi_pipeline is
+entity ftoi_pl is
     port (
       clk   : in std_logic;
-      xrst  : in std_logic;
-      stall : in std_logic;
+    --  xrst  : in std_logic;
+    --  stall : in std_logic;
       a     : in unsigned(31 downto 0);
       s     : out unsigned(31 downto 0));
-end entity ftoi_pipeline;
+end entity ftoi_pl;
 
-architecture behavior of ftoi_pipeline is
+architecture behavior of ftoi_pl is
 
   type latch_t is record
     a : unsigned(31 downto 0);
@@ -29,12 +29,12 @@ architecture behavior of ftoi_pipeline is
 
   function ftoi(a: fpu_data_t)
     return fpu_data_t is
-
     variable fa     : float_t;
     variable sign   : std_logic;
     variable diff   : integer range 0 to 128;
     variable result : fpu_data_t;
-
+    constant intmax : fpu_data_t := x"7fffffff";
+    constant nintmax : fpu_data_t := x"80000000";
   begin
 
     if is_metavalue(a) then
@@ -50,45 +50,47 @@ architecture behavior of ftoi_pipeline is
       diff := to_integer(fa.expt) - 127;
 
       if diff > 30 then
-        result := (others => '0');
+        if sign = '0' then
+          result := intmax;
+        else 
+          result := nintmax;
+        end if;  
       elsif diff < 23 then
         result := resize(shift_right("1" & fa.frac, 23 - diff), 32);
       else
-        result := resize(shift_left("1" & fa.frac, diff - 23), 32);
+        --result := resize(shift_left("1" & fa.frac, diff - 23), 32);
+        result := shift_left(resize("1" & fa.frac, 32), diff - 23);
       end if;
 
       if sign = '1' then
         result := unsigned(- signed(result));
       end if;
     end if;
-
     return result;
   end function;
 
 
 begin
 
-  comb: process (r, a, stall) is
+  comb: process (r, a) is
     variable v: latch_t;
   begin
     v := r;
 
-    if stall /= '1' then
+    --if stall /= '1' then
       v.a    := a;
       v.s    := ftoi(r.a);
-    end if;
+    --end if;
 
     s   <= r.s;
     rin <= v;
   end process comb;
 
-  seq: process (clk, xrst) is
+  seq: process (clk) is
   begin
-    if xrst = '0' then
-      r <= latch_init;
-    elsif rising_edge(clk) then
+     if rising_edge(clk) then
       r <= rin;
-    end if;
+     end if;
   end process seq;
 
 end architecture behavior;
