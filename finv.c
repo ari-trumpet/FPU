ca4f8f9da7e2ebb685b0376530313b2e11e07fe5
ari-trumpet <brassband_160trumpet@yahoo.co.jp>
2016-02-07 18:48:59 +0900

    Update finv.c

--- finv.c
+++ finv.c
@@ -11,9 +11,9 @@
 #define MASK11 8384512  //((1 << 11) - 1) << 12
 #define MASK10 8380416
 
-uint32_t fadd(uint32_t a, uint32_t b);
+//uint32_t fadd(uint32_t a, uint32_t b);
 
-uint32_t fmul(uint32_t a, uint32_t b);
+//uint32_t fmul(uint32_t a, uint32_t b);
 
 
 static double make_a(double t, double c) {
@@ -107,74 +107,3 @@ uint32_t finv(uint32_t org) {
   return (result.uint32);
 }
 
-
-
-
-
-uint32_t finv_fast(uint32_t org) {   //テーブル化する部分
-  double a_db,b_db,c,t;
-  unsigned int index;
-  union data_32bit a,b,fraction,result,original;
-  int d;
-  
-  original.uint32 = org;
-
-  if (original.exp == 255 && original.frac != 0) {
-    result.sign = 0;
-    result.exp  = 255;
-    result.frac = FRAC_MAX; // NaN -> NaN
-  } else if (original.sign == 0 && original.exp == 0) {
-    result.uint32 = INF;    //非正規仮数(正)は0として処理。要修正
-  } else if (original.sign == 1 && original.exp == 0) {
-    result.uint32 = NINF;   //非正規仮数(負)は-0として処理
-  } else if (original.uint32 == INF) {
-    result.uint32 = ZERO;
-  } else if (original.uint32 == NINF) {
-    result.uint32 = NZERO;
-  } else {
-
-
-    if (original.frac == 0) {
-      result.uint32 = org;
-      if (original.exp >= 127) {
-	d = original.exp - 127;
-	result.exp = 127 - d;
-      } else {
-	d = 127 - original.exp;
-	result.exp = 127 + d;
-      }
-    } else {
-      fraction.uint32 = org;
-      fraction.exp = 127;
-      fraction.sign = 0;
-
-/*  直線補間による逆数演算  */
-
-      c = 1.0 / MAX;
-      index = (fraction.frac & MASK11) >> 12;   //テーブルサイズが11bit
-      t = 1 + (c * index);
-      a_db = make_a(t,c) * (-1);
-      b_db = make_b(t,c);
-      a.fl32 = (float)a_db;
-      b.fl32 = (float)b_db;
-      fraction.uint32 = fadd(fmul(a.uint32, fraction.uint32), b.uint32);
-    
-      result.sign = original.sign;
-      if (original.exp >= 127) {
-	d = original.exp - 127;
-	if (d < 126) {  // d = 128:already excluded , 127,126:非正規化数->ZEROとして扱う  
-	  result.exp = 127 - d - 1;
-	  result.frac = fraction.frac;
-	} else {
-	  result.exp = 0;
-	  result.frac = 0;
-	}
-      } else {
-	d = 127 - original.exp;
-	result.exp = 127 + d - 1;
-	result.frac = fraction.frac;
-      }
-    }
-  }
-  return (result.uint32);
-}
